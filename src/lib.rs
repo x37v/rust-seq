63db9faf35d41e37dcdbf26fb9d4c15820c88c57
Alex Norman <alex@x37v.info>
2018-05-01 21:18:22 -0700

    export List and Node, ditch real cache [moved to integration]

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[doc(hidden)]
 pub extern crate xnor_llist;
 
-use xnor_llist::{List, Node};
+pub use xnor_llist::{List, Node};
 
 use std::thread;
 use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
@@ -289,7 +289,6 @@ where
 mod tests {
     use super::*;
     use std::thread;
-    use std::sync::mpsc::{sync_channel, Receiver, SyncSender, TrySendError};
 
     #[test]
     fn can_vec() {
@@ -328,86 +327,6 @@ mod tests {
         s.schedule(
             TimeSched::Absolute(0),
             Box::new(move |s: &mut ExecSched<()>| {
-                println!("Closure in schedule");
-                assert!(s.cache().pop_node().is_some());
-                TimeResched::Relative(3)
-            }),
-        );
-
-        let child = thread::spawn(move || {
-            let mut e = e.unwrap();
-            e.run(32);
-            e.run(32);
-        });
-
-        assert!(child.join().is_ok());
-    }
-
-    struct TestCache {
-        receiver: Receiver<SchedFnNode<TestCache>>,
-    }
-
-    struct TestCacheUpdater {
-        sender: SyncSender<SchedFnNode<TestCache>>,
-    }
-
-    struct TestCacheCreator {
-        sender: SyncSender<SchedFnNode<TestCache>>,
-        cache: Option<TestCache>,
-    }
-
-    impl NodeCache<TestCache> for TestCache {
-        fn pop_node(&mut self) -> Option<SchedFnNode<TestCache>> {
-            self.receiver.try_recv().ok()
-        }
-    }
-
-    impl CacheUpdate for TestCacheUpdater {
-        fn update(&mut self) -> bool {
-            loop {
-                let f = Node::new_boxed(Default::default());
-                match self.sender.try_send(f) {
-                    Ok(_) => {}
-                    Err(TrySendError::Full(_)) => return true,
-                    Err(TrySendError::Disconnected(_)) => return false,
-                }
-            }
-        }
-    }
-
-    impl CacheCreate<TestCache, TestCacheUpdater> for TestCacheCreator {
-        fn cache(&mut self) -> Option<TestCache> {
-            self.cache.take()
-        }
-
-        fn updater(&mut self) -> Option<TestCacheUpdater> {
-            Some(TestCacheUpdater {
-                sender: self.sender.clone(),
-            })
-        }
-    }
-
-    impl Default for TestCacheCreator {
-        fn default() -> Self {
-            let (sender, receiver) = sync_channel(1024);
-            TestCacheCreator {
-                sender: sender,
-                cache: Some(TestCache { receiver: receiver }),
-            }
-        }
-    }
-
-    #[test]
-    fn real_cache() {
-        type SImpl = Scheduler<TestCacheCreator, TestCache, TestCacheUpdater>;
-        let mut s = SImpl::new();
-        s.spawn_helper_threads();
-
-        let e = s.executor();
-        assert!(e.is_some());
-        s.schedule(
-            TimeSched::Absolute(0),
-            Box::new(move |s: &mut ExecSched<TestCache>| {
                 println!("Closure in schedule");
                 assert!(s.cache().pop_node().is_some());
                 TimeResched::Relative(3)
