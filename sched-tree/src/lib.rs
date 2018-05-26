14a22cd1076233e0afc076a2dfd69a31c6e861a5
Alex Norman <alex@x37v.info>
2018-05-26 10:54:44 -0700

    quick clock struct

--- sched-tree/src/lib.rs
+++ sched-tree/src/lib.rs
@@ -1,3 +1,31 @@
+extern crate sched;
+
+use sched::{ExecSched, SchedCall, SchedFn, TimeResched};
+
+pub struct Clock<SrcSnk, Context> {
+    tick_period: usize,
+    sched: SchedFn<SrcSnk, Context>,
+}
+
+impl<SrcSnk, Context> SchedCall<SrcSnk, Context> for Clock<SrcSnk, Context> {
+    fn sched_call(
+        &mut self,
+        s: &mut ExecSched<SrcSnk, Context>,
+        context: &mut Context,
+    ) -> TimeResched {
+        match self.sched.sched_call(s, context) {
+            TimeResched::None => TimeResched::None,
+            _ => TimeResched::ContextRelative(self.tick_period),
+        }
+    }
+}
+
+impl<SrcSnk, Context> Clock<SrcSnk, Context> {
+    pub fn new(tick_period: usize, sched: SchedFn<SrcSnk, Context>) -> Self {
+        Clock { tick_period, sched }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
