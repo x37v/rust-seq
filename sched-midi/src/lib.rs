302588e31c94d5e52f54def86ec024b51d106793
Alex Norman <alex@x37v.info>
2018-10-07 07:50:06 -0700

    iterator, status codes

--- sched-midi/src/lib.rs
+++ sched-midi/src/lib.rs
@@ -6,6 +6,7 @@ use sched::TimeSched;
 use std::sync::mpsc::SyncSender;
 use std::sync::Arc;
 
+#[derive(Clone, Copy, Debug)]
 pub enum MidiValue {
     Note {
         on: bool,
@@ -15,9 +16,91 @@ pub enum MidiValue {
     },
 }
 
+#[derive(Clone, Copy, Debug)]
+#[repr(u8)]
+pub enum MidiStatus {
+    NoteOn = 0x90,
+    NoteOff = 0x80,
+    AfterTouch = 0xA0,
+    ContCtrl = 0xB0,
+    ProgChng = 0xC0,
+    ChanPres = 0xD0,
+    PitchBend = 0xE0,
+
+    Clock = 0xF8,
+    Tick = 0xF9,
+    Start = 0xFA,
+    Continue = 0xFB,
+    Stop = 0xFC,
+    ActiveSense = 0xFE,
+    Reset = 0xFF,
+
+    TcQFrame = 0xF1,
+    SongPos = 0xF2,
+    SongSel = 0xF3,
+    TuneReq = 0xF6,
+
+    SysexBeg = 0xF0,
+    SysexEnd = 0xF7,
+}
+
+impl From<MidiStatus> for u8 {
+    fn from(v: MidiStatus) -> u8 {
+        v as u8
+    }
+}
+
 pub struct MidiValueAt {
     value: MidiValue,
     tick: usize,
+}
+
+pub struct MidiValueIterator<'a> {
+    value: &'a MidiValue,
+    index: u8,
+}
+
+impl MidiValue {
+    pub fn iter(&self) -> MidiValueIterator {
+        MidiValueIterator {
+            value: self,
+            index: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for MidiValueIterator<'a> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<u8> {
+        let r = match self.value {
+            MidiValue::Note { on, chan, num, vel } => match self.index {
+                0 => Some(
+                    chan & 0x0F | if *on {
+                        MidiStatus::NoteOn
+                    } else {
+                        MidiStatus::NoteOff
+                    } as u8,
+                ),
+                1 => Some(num & 0x7F),
+                2 => Some(vel & 0x7F),
+                _ => None,
+            },
+        };
+        //so we never overflow
+        if r.is_some() {
+            self.index += 1;
+        }
+        r
+    }
+}
+
+impl<'a> ExactSizeIterator for MidiValueIterator<'a> {
+    fn len(&self) -> usize {
+        match self.value {
+            MidiValue::Note { .. } => 3,
+        }
+    }
 }
 
 impl MidiValueAt {
