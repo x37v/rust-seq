d802c75c27113df2c3bbaeb8cab2b252deae2630
Alex Norman <alex@x37v.info>
2018-11-18 12:26:52 -0800

    more generic wrapper, get impl, deref impl, tests

--- src/observable_binding.rs
+++ src/observable_binding.rs
@@ -1,6 +1,8 @@
 use base::{LList, LNode};
+use binding::ParamBindingGet;
 use binding::ParamBindingSet;
 use std::marker::PhantomData;
+use std::ops::Deref;
 use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
 use std::sync::mpsc::SyncSender;
 
@@ -22,16 +24,16 @@ pub fn new_observer_node(sender: SyncSender<ObservableId>) -> ObserverNode {
     LNode::new_boxed(sender)
 }
 
-pub struct ObservableBindingSet<B, T> {
+pub struct ObservableBinding<B, T> {
     id: ObservableId,
     binding: T,
     observers: spinlock::Mutex<ObserverList>,
     _phantom: PhantomData<AtomicPtr<Box<B>>>, //XXX used atomic so we can share across threads, could have been mutex..
 }
 
-impl<B, T> ObservableBindingSet<B, T>
+impl<B, T> ObservableBinding<B, T>
 where
-    T: ParamBindingSet<B>,
+    B: Send + Copy,
 {
     pub fn new(binding: T) -> Self {
         Self {
@@ -55,7 +57,7 @@ where
     }
 }
 
-impl<B, T> ParamBindingSet<B> for ObservableBindingSet<B, T>
+impl<B, T> ParamBindingSet<B> for ObservableBinding<B, T>
 where
     B: Copy + Send,
     T: ParamBindingSet<B>,
@@ -63,6 +65,24 @@ where
     fn set(&self, value: B) {
         self.binding.set(value);
         self.notify();
+    }
+}
+
+impl<B, T> ParamBindingGet<B> for ObservableBinding<B, T>
+where
+    B: Copy + Send,
+    T: ParamBindingGet<B>,
+{
+    fn get(&self) -> B {
+        self.binding.get()
+    }
+}
+
+impl<B, T> Deref for ObservableBinding<B, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.binding
     }
 }
 
@@ -77,49 +97,81 @@ mod tests {
 
     #[test]
     fn id_expectation() {
-        let mut u = ObservableBindingSet::new(AtomicUsize::new(2));
+        let mut u: ObservableBinding<usize, _> = ObservableBinding::new(AtomicUsize::new(2));
         let mut id = u.id.0;
 
-        u = ObservableBindingSet::new(AtomicUsize::new(3));
+        u = ObservableBinding::new(AtomicUsize::new(3));
+        assert_eq!(3, u.get());
         assert!(u.id.0 > id);
         id = u.id.0;
 
-        let i = ObservableBindingSet::new(AtomicIsize::new(7));
+        let i: ObservableBinding<isize, _> = ObservableBinding::new(AtomicIsize::new(7));
         assert!(i.id.0 > id);
         id = i.id.0;
 
-        let b = ObservableBindingSet::new(SpinlockParamBinding::new(false));
+        let b: ObservableBinding<bool, _> =
+            ObservableBinding::new(SpinlockParamBinding::new(false));
         assert!(b.id.0 > id);
+
+        assert_eq!(false, b.get());
         id = b.id.0;
 
+        b.set(true);
+        assert_eq!(id, b.id.0);
+        assert_eq!(true, b.get());
+
         let child = thread::spawn(move || {
-            let mut u = ObservableBindingSet::new(AtomicUsize::new(2));
+            let mut u: ObservableBinding<usize, _> = ObservableBinding::new(AtomicUsize::new(2));
             assert!(u.id.0 > id);
             id = u.id.0;
 
-            u = ObservableBindingSet::new(AtomicUsize::new(3));
+            u = ObservableBinding::new(AtomicUsize::new(3));
             assert!(u.id.0 > id);
             id = u.id.0;
 
-            let i = ObservableBindingSet::new(AtomicIsize::new(7));
+            let i: ObservableBinding<isize, _> = ObservableBinding::new(AtomicIsize::new(7));
             assert!(i.id.0 > id);
+            assert_eq!(7, i.get());
             id = i.id.0;
 
-            let b = ObservableBindingSet::new(SpinlockParamBinding::new(false));
+            let b: ObservableBinding<bool, _> =
+                ObservableBinding::new(SpinlockParamBinding::new(false));
             assert!(b.id.0 > id);
             id = b.id.0;
         });
         assert!(child.join().is_ok());
 
-        u = ObservableBindingSet::new(AtomicUsize::new(3));
+        u = ObservableBinding::new(AtomicUsize::new(3));
         assert!(u.id.0 > id);
+    }
+
+    #[test]
+    fn deref() {
+        let u: ObservableBinding<usize, _> = ObservableBinding::new(AtomicUsize::new(2));
+        assert_eq!(2, u.deref().get());
+        assert_eq!(2, u.get());
+
+        u.set(5);
+        assert_eq!(5, u.deref().get());
+        assert_eq!(5, u.get());
+
+        u.deref().set(23);
+        assert_eq!(23, u.deref().get());
+        assert_eq!(23, u.get());
+
+        assert_eq!(23, (*u).get());
+        assert_eq!(23, u.get());
+
+        (*u).set(42);
+        assert_eq!(42, (*u).get());
+        assert_eq!(42, u.get());
     }
 
     #[test]
     fn observe() {
         let (s1, r1) = sync_channel(16);
 
-        let mut u = ObservableBindingSet::new(AtomicUsize::new(2));
+        let mut u: ObservableBinding<usize, _> = ObservableBinding::new(AtomicUsize::new(2));
         let id = u.id;
 
         assert!(r1.try_recv().is_err());
@@ -132,19 +184,23 @@ mod tests {
         u.set(23);
         assert_eq!(id, r1.try_recv().unwrap());
         assert!(r1.try_recv().is_err());
+        assert_eq!(23, u.get());
 
         u.set(11);
         assert_eq!(id, r1.try_recv().unwrap());
         assert!(r1.try_recv().is_err());
+        assert_eq!(11, u.get());
 
         let (s2, r2) = sync_channel(16);
         assert!(r1.try_recv().is_err());
         assert!(r2.try_recv().is_err());
+        assert_eq!(11, u.get());
 
         u.set(11);
         assert_eq!(id, r1.try_recv().unwrap());
         assert!(r1.try_recv().is_err());
         assert!(r2.try_recv().is_err());
+        assert_eq!(11, u.get());
 
         u.add_observer(new_observer_node(s2));
 
@@ -153,5 +209,12 @@ mod tests {
         assert_eq!(id, r2.try_recv().unwrap());
         assert!(r1.try_recv().is_err());
         assert!(r2.try_recv().is_err());
+        assert_eq!(80, u.get());
+
+        //deref doesn't signal
+        u.deref().set(800);
+        assert!(r1.try_recv().is_err());
+        assert!(r2.try_recv().is_err());
+        assert_eq!(800, u.get());
     }
 }
