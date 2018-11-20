f93080d81cbdafe4d816c14d78eb3fe40d99ea88
Alex Norman <alex@x37v.info>
2018-11-19 21:44:18 -0800

    more generic

--- src/binding_cache.rs
+++ src/binding_cache.rs
@@ -2,6 +2,7 @@ use binding::{ParamBinding, SpinlockParamBinding};
 use failure::Fail;
 use std::any::Any;
 use std::collections::HashMap;
+use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 
 #[derive(Debug, Fail)]
@@ -19,27 +20,52 @@ impl BindingCache {
         BindingCache(Default::default())
     }
 
-    pub fn get<T>(&mut self, key: String, default: T) -> Result<Arc<dyn ParamBinding<T>>, GetError>
+    fn get_item<T, U>(
+        &mut self,
+        key: String,
+        default: U,
+    ) -> Result<Arc<dyn ParamBinding<U>>, GetError>
     where
-        T: Send + Copy + 'static,
+        T: Default + ParamBinding<U> + 'static,
     {
         if let Some(v) = self.0.get_mut(&key) {
-            if let Some(b) = v.downcast_mut::<Arc<SpinlockParamBinding<T>>>() {
+            if let Some(b) = v.downcast_mut::<Arc<T>>() {
                 Ok(b.clone())
             } else {
                 Err(GetError { key: key })
             }
         } else {
-            let v = Arc::new(SpinlockParamBinding::new(default));
-            self.0.insert(key, Box::new(v.clone()));
+            let v: Arc<T> = Arc::new(Default::default());
+            v.set(default);
+
+            let b: Box<Arc<T>> = Box::new(v.clone());
+            self.0.insert(key, b);
             Ok(v)
         }
+    }
+
+    pub fn get_usize(
+        &mut self,
+        key: String,
+        default: usize,
+    ) -> Result<Arc<dyn ParamBinding<usize>>, GetError> {
+        self.get_item::<AtomicUsize, usize>(key, default)
+    }
+
+    pub fn get<T>(&mut self, key: String, default: T) -> Result<Arc<dyn ParamBinding<T>>, GetError>
+    where
+        T: Send + Copy + Default + 'static,
+    {
+        self.get_item::<SpinlockParamBinding<T>, T>(key, default)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use binding::bpm::{
+        Clock, ClockBPMBinding, ClockData, ClockPPQBinding, ClockPeriodMicroBinding,
+    };
 
     #[test]
     fn cache() {
@@ -72,4 +98,32 @@ mod tests {
         let v = v.unwrap();
         assert_eq!(53f32, v.get());
     }
+
+    /*
+    #[test]
+    fn cache_bpm() {
+        let mut c = BindingCache::new();
+        let f = c.get::<f32>("soda".to_string(), 43f32);
+        let b = c.get::<::binding::bpm::ClockData>(
+            "bpm".to_string(),
+            ::binding::bpm::ClockData::new(110f32, 990),
+        );
+    
+        assert!(f.is_ok());
+        assert!(b.is_ok());
+        let f = f.unwrap();
+        let b = b.unwrap();
+        assert_eq!(43f32, f.get());
+        assert_eq!(110f32, b.get().bpm());
+        assert_eq!(990, b.get().ppq());
+    
+        let b2 = c.get::<ClockData>("bpm".to_string(), ClockData::new(1f32, 10));
+        assert!(b2.is_ok());
+        let b2 = b2.unwrap();
+    
+        let bpm = Arc::new(ClockBPMBinding(b2.clone()));
+        let ppq = Arc::new(ClockPPQBinding(b2.clone()));
+        let micros = Arc::new(ClockPeriodMicroBinding(b2.clone()));
+    }
+    */
 }
