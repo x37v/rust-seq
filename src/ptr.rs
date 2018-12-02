c7476ba06b5bbb3f605db057d151f8a51d9ae148
Alex Norman <alex@x37v.info>
2018-12-01 22:08:28 -0800

    notes

--- src/ptr.rs
+++ src/ptr.rs
@@ -1,3 +1,8 @@
+/// A unique ptr
 pub type UniqPtr<T> = Box<T>;
+
+/// A shared ptr
 pub type ShrPtr<T> = std::sync::Arc<T>;
+
+/// A sync/send mutable shared ptr
 pub type SShrPtr<T> = std::sync::Arc<spinlock::Mutex<T>>;
