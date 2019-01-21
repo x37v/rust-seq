e69c9ece574d9f2e8a84ffad22da053346c14d8b
Alex Norman <alex@x37v.info>
2019-01-21 14:47:07 -0800

    first pass at no_std ptr ??

--- src/ptr.rs
+++ src/ptr.rs
@@ -1,8 +1,21 @@
-/// A unique ptr
-pub type UniqPtr<T> = Box<T>;
+cfg_if! {
+    if #[cfg(feature = "std")] {
+        /// A unique ptr
+        pub type UniqPtr<T> = Box<T>;
 
-/// A shared ptr
-pub type ShrPtr<T> = std::sync::Arc<T>;
+        /// A shared ptr
+        pub type ShrPtr<T> = std::sync::Arc<T>;
 
-/// A sync/send mutable shared ptr
-pub type SShrPtr<T> = std::sync::Arc<spinlock::Mutex<T>>;
+        /// A sync/send mutable shared ptr
+        pub type SShrPtr<T> = std::sync::Arc<spinlock::Mutex<T>>;
+    } else {
+        /// A unique ptr
+        pub type UniqPtr<T> = &'static T;
+
+        /// A shared ptr
+        pub type ShrPtr<T> = &'static T;
+
+        /// A sync/send mutable shared ptr
+        pub type SShrPtr<T> = &'static mut ::spinlock::Mutex<T>;
+    }
+}
