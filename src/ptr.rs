1d1ac1402d0e349f6f1685a45b8b4c8efbdbec6e
Alex Norman <alex@x37v.info>
2019-01-21 21:23:42 -0800

    start of an attempt to make an static struct that has enough of the Arc API to use in its place

--- src/ptr.rs
+++ src/ptr.rs
@@ -11,6 +11,16 @@ cfg_if! {
     } else {
         extern crate alloc;
 
+        struct Src<T> { //static, fake reference counted
+            data: &'static mut T
+        }
+
+        impl<T> Clone for Src<T> {
+            fn clone(&self) -> Src<T> {
+                Src { data: self.data }
+            }
+        }
+
         /// A unique ptr
         pub type UniqPtr<T> = alloc::boxed::Box<T>;
 
