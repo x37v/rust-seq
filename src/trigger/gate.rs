e2d37f8db0daab81ac7d21d2a13b83a5dd411153
Alex Norman <alex@x37v.info>
2019-02-01 06:49:12 -0800

    remove need for vec

--- src/trigger/gate.rs
+++ src/trigger/gate.rs
@@ -1,17 +1,22 @@
 use crate::binding::ParamBindingGet;
-use crate::ptr::{SShrPtr, ShrPtr};
+use crate::ptr::SShrPtr;
 use crate::trigger::*;
 
 /// A trigger that evaluates its children only when the associated gate value is true
-pub struct TriggerGate<T> {
+pub struct TriggerGate<Gate, Children>
+where
+    Gate: ParamBindingGet<bool>,
+    for<'a> &'a Children: core::iter::IntoIterator<Item = &'a SShrPtr<dyn Trigger>>,
+{
     trigger_index: TriggerId,
-    gate: ShrPtr<T>,
-    children: Vec<SShrPtr<dyn Trigger>>,
+    gate: Gate,
+    children: Children,
 }
 
-impl<T> TriggerGate<T>
+impl<Gate, Children> TriggerGate<Gate, Children>
 where
-    T: ParamBindingGet<bool>,
+    Gate: ParamBindingGet<bool>,
+    for<'a> &'a Children: core::iter::IntoIterator<Item = &'a SShrPtr<dyn Trigger>>,
 {
     /// Construct a new `TriggerGate`
     ///
@@ -19,7 +24,7 @@ where
     ///
     /// * `gate` - the binding value for the gate
     /// * `children` - a list of children to trigger
-    pub fn new(gate: ShrPtr<T>, children: Vec<SShrPtr<dyn Trigger>>) -> Self {
+    pub fn new(gate: Gate, children: Children) -> Self {
         Self {
             trigger_index: TriggerId::new(),
             gate,
@@ -28,9 +33,10 @@ where
     }
 }
 
-impl<T> Trigger for TriggerGate<T>
+impl<Gate, Children> Trigger for TriggerGate<Gate, Children>
 where
-    T: ParamBindingGet<bool>,
+    Gate: ParamBindingGet<bool>,
+    for<'a> &'a Children: core::iter::IntoIterator<Item = &'a SShrPtr<dyn Trigger>>,
 {
     fn trigger_index(&self) -> TriggerId {
         self.trigger_index
@@ -38,9 +44,22 @@ where
 
     fn trigger_eval(&self, tick: usize, context: &mut dyn ScheduleTrigger) {
         if self.gate.get() {
-            for t in self.children.iter() {
+            for t in self.children.into_iter() {
                 t.lock().trigger_eval(tick, context);
             }
         }
+    }
+}
+
+#[cfg(all(test, feature = "with_std"))]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn clamp() {
+        let g = false;
+        let v: Vec<SShrPtr<dyn Trigger>> = Vec::new();
+
+        let tg = TriggerGate::new(g, v);
     }
 }
