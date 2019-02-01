5137fcecf7185528f725451426857ca9147139a7
Alex Norman <alex@x37v.info>
2019-01-31 19:49:03 -0800

    remove ptr dep

--- src/graph/gate.rs
+++ src/graph/gate.rs
@@ -1,23 +1,30 @@
 use super::*;
+use crate::binding::ParamBindingGet;
 
 /// A graph node that executes its children only when its binding evaluates to true.
 #[derive(GraphNode)]
-pub struct Gate {
-    binding: BindingGetP<bool>,
+pub struct Gate<B: ParamBindingGet<bool>> {
+    binding: B,
 }
 
-impl Gate {
+impl<B> Gate<B>
+where
+    B: ParamBindingGet<bool>,
+{
     /// Construct a new `Gate`
     ///
     /// # Arguments
     ///
     /// * `binding` - the binding which determines if the gate is open or closed
-    pub fn new(binding: BindingGetP<bool>) -> Self {
+    pub fn new(binding: B) -> Self {
         Self { binding }
     }
 }
 
-impl GraphNodeExec for Gate {
+impl<B> GraphNodeExec for Gate<B>
+where
+    B: ParamBindingGet<bool>,
+{
     fn exec_node(&mut self, context: &mut dyn SchedContext, children: &mut dyn ChildExec) {
         if self.binding.get() {
             children.exec_all(context);
