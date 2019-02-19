b6e63ec193c4bece54d2a45ce6486f06dc6d84cb
Alex Norman <alex@x37v.info>
2019-02-18 17:11:44 -0800

    trig call struct, should be usable non std

--- src/trigger.rs
+++ src/trigger.rs
@@ -1,4 +1,6 @@
 use crate::binding::set::BindingSet;
+use crate::binding::ParamBindingLatch;
+use crate::list::LinkedList;
 use crate::time::{TimeResched, TimeSched};
 use core::sync::atomic::{AtomicUsize, Ordering};
 
@@ -22,6 +24,36 @@ static ID_COUNT: AtomicUsize = AtomicUsize::new(0);
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
 pub struct TriggerId {
     id: usize,
+}
+
+pub struct TrigCall<VL>
+where
+    VL: LinkedList<BindingSet>,
+    for<'a> &'a VL: core::iter::IntoIterator<Item = &'a mut BindingSet>,
+{
+    index: Option<TriggerId>,
+    values: VL,
+}
+
+impl<VL> TrigCall<VL>
+where
+    VL: LinkedList<BindingSet>,
+    for<'a> &'a VL: core::iter::IntoIterator<Item = &'a mut BindingSet>,
+{
+    pub fn set_index(&mut self, index: Option<TriggerId>) {
+        self.index = index;
+    }
+    pub fn index(&self) -> Option<TriggerId> {
+        self.index
+    }
+    pub fn add_value(&mut self, binding: BindingSet) {
+        self.values.push_back(binding);
+    }
+    pub fn latch_values(&mut self) {
+        for v in &self.values {
+            v.store()
+        }
+    }
 }
 
 impl TriggerId {
