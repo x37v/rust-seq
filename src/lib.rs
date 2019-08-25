f734c2ad2bb6bca17a780313308218fb1fd68f96
Alex Norman <alex@x37v.info>
2019-08-25 09:05:15 -0700

    include std and no std impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 pub mod event;
 pub mod item_source;
 pub mod time;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "std")] {
+        pub mod std;
+    } else {
+        pub mod no_std;
+    }
+}
