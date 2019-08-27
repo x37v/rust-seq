b474131c48966e28a07532bf0453abebde433687
Alex Norman <alex@x37v.info>
2019-08-26 21:03:21 -0700

    updated trait, returns a different success type

--- src/item_source.rs
+++ src/item_source.rs
@@ -1,4 +1,4 @@
 /// A source for items of type T that can be sent across threads
-pub trait ItemSource<T>: Send + Sync {
-    fn try_pop(&mut self) -> Result<T, core::fmt::Error>;
+pub trait ItemSource<T, O>: Send + Sync {
+    fn try_get(&mut self, init: T) -> Result<O, T>;
 }
