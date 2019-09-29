07658eed09e4fa2728a2f073f74873e8944e682c
Alex Norman <alex@x37v.info>
2019-09-29 08:12:59 -0700

    add helper struct for inserting into priority queue

--- src/pqueue.rs
+++ src/pqueue.rs
@@ -1,4 +1,10 @@
 extern crate alloc;
+use core::cmp::Ordering;
+
+pub struct TickItem<T> {
+    tick: usize,
+    item: T,
+}
 
 pub trait TickPriorityEnqueue<T>: Send {
     fn enqueue(&mut self, tick: usize, value: T) -> Result<(), T>;
@@ -26,3 +32,48 @@ where
         self.lock().dequeue_lt(tick)
     }
 }
+
+impl<T> core::convert::From<(usize, T)> for TickItem<T> {
+    fn from(item: (usize, T)) -> Self {
+        Self {
+            tick: item.0,
+            item: item.1,
+        }
+    }
+}
+
+impl<T> core::convert::Into<(usize, T)> for TickItem<T> {
+    fn into(self) -> (usize, T) {
+        (self.tick, self.item)
+    }
+}
+
+//impl Ord so we can use this in a pqueue
+impl<T> Ord for TickItem<Box<T>> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.tick.cmp(&other.tick) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Equal => {
+                //if ticks are equal, sort by pointer
+                let left: *const T = self.item.as_ref();
+                let right: *const T = other.item.as_ref();
+                left.cmp(&right)
+            }
+        }
+    }
+}
+
+impl<T> PartialOrd for TickItem<Box<T>> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T> PartialEq for TickItem<Box<T>> {
+    fn eq(&self, _other: &Self) -> bool {
+        false
+    }
+}
+
+impl<T> Eq for TickItem<Box<T>> {}
