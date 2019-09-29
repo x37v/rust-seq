ac3499e54d7b15a0f0cdd07a9479c2d06062ab81
Alex Norman <alex@x37v.info>
2019-09-29 12:53:35 -0700

    auto impl for arc and ref

--- src/item_sink.rs
+++ src/item_sink.rs
@@ -1,4 +1,24 @@
+extern crate alloc;
+
 /// A sink for items of type T that can be sent across threads
 pub trait ItemSink<T>: Send + Sync {
     fn try_put(&mut self, item: T) -> Result<(), T>;
+}
+
+impl<T> ItemSink<T> for alloc::sync::Arc<spin::Mutex<dyn ItemSink<T>>>
+where
+    T: Send,
+{
+    fn try_put(&mut self, item: T) -> Result<(), T> {
+        self.lock().try_put(item)
+    }
+}
+
+impl<T> ItemSink<T> for &'static spin::Mutex<dyn ItemSink<T>>
+where
+    T: Send,
+{
+    fn try_put(&mut self, item: T) -> Result<(), T> {
+        self.lock().try_put(item)
+    }
 }
