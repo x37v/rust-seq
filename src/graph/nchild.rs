44efc4b5fd6b5c2826e401b84e4ed6efe8112d3d
Alex Norman <alex@x37v.info>
2019-10-08 20:16:46 -0700

    nchild impl

--- src/graph/nchild.rs
+++ src/graph/nchild.rs
@@ -1,8 +1,7 @@
 use crate::event::*;
-use crate::graph::{ChildCount, GraphChildExec, GraphNode, GraphNodeContainer};
-use core::marker::PhantomData;
+use crate::graph::{ChildCount, GraphChildExec, GraphNode};
 
-/// A graph node that fakes that it has infinite children.
+/// A graph node children impl that fakes that it has infinite children.
 /// It has 'index' children that get called each time a child is addressed by index.
 
 pub trait GraphIndexExec: Send {
@@ -18,7 +17,6 @@ cfg_if::cfg_if! {
     }
 }
 
-/*
 impl GraphIndexExec for IndexChildContainer {
     fn exec_index(&mut self, index: usize, context: &mut dyn EventEvalContext) {
         self.0.lock().exec_index(index, context);
@@ -27,12 +25,18 @@ impl GraphIndexExec for IndexChildContainer {
 
 pub struct IndexChildren([IndexChildContainer]);
 
-pub struct NChildWrapper {
-    child: GraphNodeContainer,
+pub struct NChildWrapper<N>
+where
+    N: GraphNode,
+{
+    child: N,
     index_children: IndexChildren,
 }
 
-impl GraphChildExec for NChildWrapper {
+impl<N> GraphChildExec for NChildWrapper<N>
+where
+    N: GraphNode,
+{
     fn child_count(&self) -> ChildCount {
         ChildCount::Inf
     }
@@ -41,12 +45,11 @@ impl GraphChildExec for NChildWrapper {
         context: &mut dyn EventEvalContext,
         range: core::ops::Range<usize>,
     ) {
-        self.child.exec(context);
         for i in range {
+            self.child.node_exec(context);
             for ic in &mut self.index_children.0 {
                 ic.exec_index(i, context);
             }
         }
     }
 }
-*/
