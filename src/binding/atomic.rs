5da2fac440b988509425625f26c96567de7fa6cc
Alex Norman <alex@x37v.info>
2019-10-28 08:18:15 -0700

    conditionally add 64 bit atomics

--- src/binding/atomic.rs
+++ src/binding/atomic.rs
@@ -21,10 +21,14 @@ impl_get_set!(bool, AtomicBool);
 impl_get_set!(i8, AtomicI8);
 impl_get_set!(i16, AtomicI16);
 impl_get_set!(i32, AtomicI32);
-impl_get_set!(i64, AtomicI64);
 impl_get_set!(isize, AtomicIsize);
 impl_get_set!(u8, AtomicU8);
 impl_get_set!(u16, AtomicU16);
 impl_get_set!(u32, AtomicU32);
-impl_get_set!(u64, AtomicU64);
 impl_get_set!(usize, AtomicUsize);
+
+//requires 64bit pointer size, should be able to enable for other 64bit targets..
+#[cfg(target_arch = "x86_64")]
+impl_get_set!(i64, AtomicI64);
+#[cfg(target_arch = "x86_64")]
+impl_get_set!(u64, AtomicU64);
