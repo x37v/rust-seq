138e62887974caba02f36d124e118aae77919c53
Alex Norman <alex@x37v.info>
2020-11-28 10:26:03 -0800

    just use get/set, impl trait, test

--- src/binding/last.rs
+++ src/binding/last.rs
@@ -3,7 +3,20 @@
 //! Mostly useful for wrapping generators so that we can observe what value has been used without
 //! altering it.
 
-use crate::binding::{ParamBinding, ParamBindingGet, ParamBindingSet};
+use crate::binding::{ParamBindingGet, ParamBindingSet};
+
+/// Trait for cached get and or set values.
+pub trait BindingLast<T> {
+    /// Get the last value that the binding gave, if there has been one.
+    fn last_get(&self) -> Option<T> {
+        None
+    }
+
+    /// Get the last value that the binding was set to, if there has been one.
+    fn last_set(&self) -> Option<T> {
+        None
+    }
+}
 
 /// Wrapper for a `ParamBindingGet`, caches the last get value so it can be observed later.
 pub struct BindingLastGet<T, B> {
@@ -17,7 +30,7 @@ pub struct BindingLastSet<T, B> {
     binding: B,
 }
 
-/// Wrapper for a `ParamBinding`, caches the last get and set values so they can be observed later.
+/// Wrapper for a `ParamBindingGet` + `ParamBindingSet`, caches the last get and set values so they can be observed later.
 pub struct BindingLastGetSet<T, B> {
     last_get: spin::Mutex<Option<T>>,
     last_set: spin::Mutex<Option<T>>,
@@ -48,8 +61,14 @@ where
             binding,
         }
     }
+}
+
+impl<T, B> BindingLast<T> for BindingLastGet<T, B>
+where
+    T: Send + Copy,
+{
     /// Get the last value that the binding gave, if there has been one.
-    pub fn last_get(&self) -> Option<T> {
+    fn last_get(&self) -> Option<T> {
         *self.last_value.lock()
     }
 }
@@ -77,8 +96,14 @@ where
             binding,
         }
     }
+}
+
+impl<T, B> BindingLast<T> for BindingLastSet<T, B>
+where
+    T: Send + Copy,
+{
     /// Get the last value that the binding gave, if there has been one.
-    pub fn last_set(&self) -> Option<T> {
+    fn last_set(&self) -> Option<T> {
         *self.last_value.lock()
     }
 }
@@ -86,7 +111,7 @@ where
 impl<T, B> ParamBindingGet<T> for BindingLastGetSet<T, B>
 where
     T: Send + Copy,
-    B: ParamBinding<T>,
+    B: ParamBindingGet<T>,
 {
     fn get(&self) -> T {
         let mut g = self.last_get.lock();
@@ -99,7 +124,7 @@ where
 impl<T, B> ParamBindingSet<T> for BindingLastGetSet<T, B>
 where
     T: Send + Copy,
-    B: ParamBinding<T>,
+    B: ParamBindingSet<T>,
 {
     fn set(&self, value: T) {
         let mut g = self.last_set.lock();
@@ -120,12 +145,98 @@ where
             binding,
         }
     }
+}
+
+impl<T, B> BindingLast<T> for BindingLastGetSet<T, B>
+where
+    T: Send + Copy,
+{
+    /// Get the last value that the binding gave, if there has been one.
+    fn last_get(&self) -> Option<T> {
+        *self.last_get.lock()
+    }
     /// Get the last value that the binding was set to, if there has been one.
-    pub fn last_set(&self) -> Option<T> {
+    fn last_set(&self) -> Option<T> {
         *self.last_set.lock()
     }
-    /// Get the last value that the binding gave, if there has been one.
-    pub fn last_get(&self) -> Option<T> {
-        *self.last_get.lock()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::binding::{ParamBindingGet, ParamBindingSet};
+    use std::sync::{atomic::AtomicUsize, Arc};
+
+    #[test]
+    fn can_wrap_get() {
+        let x = Arc::new(AtomicUsize::new(0));
+        let w: Arc<BindingLastGet<usize, _>> = Arc::new(BindingLastGet::new(
+            x.clone() as Arc<dyn ParamBindingGet<usize>>
+        ));
+        assert_eq!(w.last_get(), None);
+        assert_eq!(w.get(), 0usize);
+        assert_eq!(w.last_get(), Some(0));
+        assert_eq!(w.last_get(), Some(0));
+        assert_eq!(w.get(), 0usize);
+        assert_eq!(w.last_get(), Some(0));
+
+        x.store(2084, std::sync::atomic::Ordering::SeqCst);
+        assert_eq!(w.last_get(), Some(0));
+        assert_eq!(w.get(), 2084usize);
+        assert_eq!(w.last_get(), Some(2084));
+    }
+
+    #[test]
+    fn can_wrap_set() {
+        let x = Arc::new(AtomicUsize::new(0));
+        let w: Arc<BindingLastSet<usize, _>> = Arc::new(BindingLastSet::new(
+            x.clone() as Arc<dyn ParamBindingSet<usize>>
+        ));
+        assert_eq!(w.last_set(), None);
+        w.set(0usize);
+
+        assert_eq!(w.last_set(), Some(0));
+        assert_eq!(w.last_set(), Some(0));
+
+        w.set(1usize);
+        assert_eq!(w.last_set(), Some(1));
+
+        //setting the atomic directly doesn't actually work
+        x.store(2084, std::sync::atomic::Ordering::SeqCst);
+        assert_eq!(w.last_set(), Some(1));
+
+        //but using the set interface does
+        w.set(42usize);
+        assert_eq!(w.last_set(), Some(42));
+    }
+
+    #[test]
+    fn can_wrap_get_set() {
+        let w: Arc<BindingLastGetSet<usize, _>> =
+            Arc::new(BindingLastGetSet::new(AtomicUsize::new(0)));
+        let s = w.clone() as Arc<dyn ParamBindingSet<usize>>;
+        let g = w.clone() as Arc<dyn ParamBindingGet<usize>>;
+
+        assert_eq!(w.last_get(), None);
+        assert_eq!(w.last_set(), None);
+
+        s.set(2084usize);
+        assert_eq!(w.last_get(), None);
+        assert_eq!(w.last_set(), Some(2084usize));
+
+        assert_eq!(g.get(), 2084usize);
+        assert_eq!(w.last_get(), Some(2084usize));
+        assert_eq!(w.last_set(), Some(2084usize));
+
+        assert_eq!(g.get(), 2084usize);
+        assert_eq!(w.last_get(), Some(2084usize));
+        assert_eq!(w.last_set(), Some(2084usize));
+
+        s.set(42usize);
+        assert_eq!(w.last_set(), Some(42usize));
+        assert_eq!(w.last_get(), Some(2084usize));
+        assert_eq!(g.get(), 42usize);
+        assert_eq!(w.last_set(), Some(42usize));
+        assert_eq!(w.last_get(), Some(42usize));
     }
 }
