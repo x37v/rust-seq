1494fad3593767aefbcbf468f5aa50939bb16527
Alex Norman <alex@x37v.info>
2020-12-29 08:21:00 -0800

    replace last_get/last_set with just last

--- src/binding/last.rs
+++ src/binding/last.rs
@@ -7,13 +7,8 @@ use crate::binding::{ParamBinding, ParamBindingGet, ParamBindingSet};
 
 /// Trait for cached get and or set values.
 pub trait BindingLast<T>: Send + Sync {
-    /// Get the last value that the binding gave, if there has been one.
-    fn last_get(&self) -> Option<T> {
-        None
-    }
-
-    /// Get the last value that the binding was set to, if there has been one.
-    fn last_set(&self) -> Option<T> {
+    /// Get the last value that the binding got or was set to, if there has been one.
+    fn last(&self) -> Option<T> {
         None
     }
 }
@@ -30,10 +25,9 @@ pub struct BindingLastSet<T> {
     binding: Box<dyn ParamBindingSet<T>>,
 }
 
-/// Wrapper for a `ParamBinding` (Get + Set), caches the last get and set values so they can be observed later.
+/// Wrapper for a `ParamBinding` (Get + Set), caches the last get and set value so it can be observed later.
 pub struct BindingLastGetSet<T> {
-    last_get: spin::Mutex<Option<T>>,
-    last_set: spin::Mutex<Option<T>>,
+    last_value: spin::Mutex<Option<T>>,
     binding: Box<dyn ParamBinding<T>>,
 }
 
@@ -74,7 +68,7 @@ where
     T: Send + Copy,
 {
     /// Get the last value that the binding gave, if there has been one.
-    fn last_get(&self) -> Option<T> {
+    fn last(&self) -> Option<T> {
         *self.last_value.lock()
     }
 }
@@ -104,8 +98,8 @@ impl<T> BindingLast<T> for BindingLastSet<T>
 where
     T: Send + Copy,
 {
-    /// Get the last value that the binding gave, if there has been one.
-    fn last_set(&self) -> Option<T> {
+    /// Get the last value that the binding was set to, if there has been one.
+    fn last(&self) -> Option<T> {
         *self.last_value.lock()
     }
 }
@@ -115,7 +109,7 @@ where
     T: Send + Copy,
 {
     fn get(&self) -> T {
-        let mut g = self.last_get.lock();
+        let mut g = self.last_value.lock();
         let v = self.binding.get();
         *g = Some(v);
         v
@@ -127,7 +121,7 @@ where
     T: Send + Copy,
 {
     fn set(&self, value: T) {
-        let mut g = self.last_set.lock();
+        let mut g = self.last_value.lock();
         self.binding.set(value);
         *g = Some(value);
     }
@@ -140,8 +134,7 @@ where
     /// Construct a BindingLastSet, wrapping the given binding.
     pub fn new<B: ParamBinding<T> + 'static>(binding: B) -> Self {
         Self {
-            last_get: spin::Mutex::new(None),
-            last_set: spin::Mutex::new(None),
+            last_value: spin::Mutex::new(None),
             binding: Box::new(binding),
         }
     }
@@ -151,20 +144,16 @@ impl<T> BindingLast<T> for BindingLastGetSet<T>
 where
     T: Send + Copy,
 {
-    /// Get the last value that the binding gave, if there has been one.
-    fn last_get(&self) -> Option<T> {
-        *self.last_get.lock()
-    }
-    /// Get the last value that the binding was set to, if there has been one.
-    fn last_set(&self) -> Option<T> {
-        *self.last_set.lock()
+    /// Get the last value that the binding got or was set to, if there has been one.
+    fn last(&self) -> Option<T> {
+        *self.last_value.lock()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::binding::{ParamBindingGet, ParamBindingSet};
+    use crate::binding::{ParamBinding, ParamBindingGet, ParamBindingSet};
     use std::sync::{atomic::AtomicUsize, Arc};
 
     #[test]
@@ -173,17 +162,17 @@ mod tests {
         let w: Arc<BindingLastGet<usize>> = Arc::new(BindingLastGet::new(
             x.clone() as Arc<dyn ParamBindingGet<usize>>
         ));
-        assert_eq!(w.last_get(), None);
+        assert_eq!(w.last(), None);
         assert_eq!(w.get(), 0usize);
-        assert_eq!(w.last_get(), Some(0));
-        assert_eq!(w.last_get(), Some(0));
+        assert_eq!(w.last(), Some(0));
+        assert_eq!(w.last(), Some(0));
         assert_eq!(w.get(), 0usize);
-        assert_eq!(w.last_get(), Some(0));
+        assert_eq!(w.last(), Some(0));
 
         x.store(2084, std::sync::atomic::Ordering::SeqCst);
-        assert_eq!(w.last_get(), Some(0));
+        assert_eq!(w.last(), Some(0));
         assert_eq!(w.get(), 2084usize);
-        assert_eq!(w.last_get(), Some(2084));
+        assert_eq!(w.last(), Some(2084));
     }
 
     #[test]
@@ -192,51 +181,57 @@ mod tests {
         let w: Arc<BindingLastSet<usize>> = Arc::new(BindingLastSet::new(
             x.clone() as Arc<dyn ParamBindingSet<usize>>
         ));
-        assert_eq!(w.last_set(), None);
+        assert_eq!(w.last(), None);
         w.set(0usize);
 
-        assert_eq!(w.last_set(), Some(0));
-        assert_eq!(w.last_set(), Some(0));
+        assert_eq!(w.last(), Some(0));
+        assert_eq!(w.last(), Some(0));
 
         w.set(1usize);
-        assert_eq!(w.last_set(), Some(1));
+        assert_eq!(w.last(), Some(1));
 
         //setting the atomic directly doesn't actually work
         x.store(2084, std::sync::atomic::Ordering::SeqCst);
-        assert_eq!(w.last_set(), Some(1));
+        assert_eq!(w.last(), Some(1));
 
         //but using the set interface does
         w.set(42usize);
-        assert_eq!(w.last_set(), Some(42));
+        assert_eq!(w.last(), Some(42));
     }
 
     #[test]
     fn can_wrap_get_set() {
-        let w: Arc<BindingLastGetSet<usize>> =
-            Arc::new(BindingLastGetSet::new(AtomicUsize::new(0)));
+        let i = Arc::new(AtomicUsize::new(0));
+        let w: Arc<BindingLastGetSet<usize>> = Arc::new(BindingLastGetSet::new(
+            i.clone() as Arc<dyn ParamBinding<usize>>
+        ));
         let s = w.clone() as Arc<dyn ParamBindingSet<usize>>;
         let g = w.clone() as Arc<dyn ParamBindingGet<usize>>;
 
-        assert_eq!(w.last_get(), None);
-        assert_eq!(w.last_set(), None);
+        assert_eq!(w.last(), None);
 
         s.set(2084usize);
-        assert_eq!(w.last_get(), None);
-        assert_eq!(w.last_set(), Some(2084usize));
+        assert_eq!(w.last(), Some(2084usize));
 
         assert_eq!(g.get(), 2084usize);
-        assert_eq!(w.last_get(), Some(2084usize));
-        assert_eq!(w.last_set(), Some(2084usize));
+        assert_eq!(w.last(), Some(2084usize));
 
         assert_eq!(g.get(), 2084usize);
-        assert_eq!(w.last_get(), Some(2084usize));
-        assert_eq!(w.last_set(), Some(2084usize));
+        assert_eq!(w.last(), Some(2084usize));
 
         s.set(42usize);
-        assert_eq!(w.last_set(), Some(42usize));
-        assert_eq!(w.last_get(), Some(2084usize));
+        assert_eq!(w.last(), Some(42usize));
         assert_eq!(g.get(), 42usize);
-        assert_eq!(w.last_set(), Some(42usize));
-        assert_eq!(w.last_get(), Some(42usize));
+
+        i.set(8usize);
+        assert_eq!(w.last(), Some(42usize));
+        assert_eq!(g.get(), 8usize);
+
+        assert_eq!(w.last(), Some(8usize));
+
+        i.set(9usize);
+        assert_eq!(w.last(), Some(8usize));
+        s.set(10usize);
+        assert_eq!(w.last(), Some(10usize));
     }
 }
