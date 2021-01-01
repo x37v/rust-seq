249cc8aa20b780b2273eeeb1c94cd2e29930e21e
Alex Norman <alex@x37v.info>
2021-01-01 13:06:21 -0800

    root clock, simply let it have children

--- src/graph/root_clock.rs
+++ src/graph/root_clock.rs
@@ -2,7 +2,7 @@ use crate::{
     binding::ParamBindingGet,
     context::ChildContext,
     event::{EventEval, EventEvalContext},
-    graph::GraphNode,
+    graph::GraphChildExec,
     tick::TickResched,
     Float,
 };
@@ -10,42 +10,42 @@ use crate::{
 #[cfg(not(feature = "std"))]
 use num::traits::float::FloatCore;
 
-/// A event_eval schedulable item that holds and executes a graph tree root.
-pub struct RootClock<PeriodMicros, T>
+/// A event_eval schedulable item that holds and executes a graph tree.
+pub struct RootClock<PeriodMicros, C>
 where
     PeriodMicros: ParamBindingGet<Float>,
-    T: GraphNode,
+    C: GraphChildExec,
 {
     tick: usize,
     tick_sub: Float,
     period_micros: PeriodMicros,
-    root: T,
+    children: C,
 }
 
-impl<PeriodMicros, T> RootClock<PeriodMicros, T>
+impl<PeriodMicros, C> RootClock<PeriodMicros, C>
 where
     PeriodMicros: ParamBindingGet<Float>,
-    T: GraphNode,
+    C: GraphChildExec,
 {
-    pub fn new(period_micros: PeriodMicros, root: T) -> Self {
+    pub fn new(period_micros: PeriodMicros, children: C) -> Self {
         Self {
             tick: 0,
             tick_sub: 0.0,
             period_micros,
-            root,
+            children,
         }
     }
 }
 
-impl<PeriodMicros, T> EventEval for RootClock<PeriodMicros, T>
+impl<PeriodMicros, C> EventEval for RootClock<PeriodMicros, C>
 where
     PeriodMicros: ParamBindingGet<Float>,
-    T: GraphNode,
+    C: GraphChildExec,
 {
     fn event_eval(&mut self, context: &mut dyn EventEvalContext) -> TickResched {
         let period_micros = self.period_micros.get();
         let mut ccontext = ChildContext::new(context, 0, self.tick, period_micros);
-        self.root.node_exec(&mut ccontext);
+        self.children.child_exec_all(&mut ccontext);
 
         let ctp = context.context_tick_period_micros();
         if period_micros <= 0.0 || ctp <= 0.0 {
