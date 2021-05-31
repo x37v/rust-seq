71e6bd5316c4c16276a3e2a892e6207e265b363a
Alex Norman <alex@x37v.info>
2021-05-30 19:54:31 -0700

    move data out of param

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+pub mod clock;
 pub mod context;
 pub mod event;
 pub mod graph;
--- src/param/bpm.rs
+++ src/param/bpm.rs
@@ -1,28 +1,8 @@
 use super::*;
-use crate::Float;
-
-const PERIOD_MICRO_MIN: Float = 0.001;
-const BPM_MIN: Float = 0.001;
-const PPQ_MIN: usize = 1;
-
-pub trait Clock: Send {
-    fn bpm(&self) -> Float;
-    fn set_bpm(&mut self, bpm: Float);
-
-    fn period_micros(&self) -> Float;
-    fn set_period_micros(&mut self, period_micros: Float);
-
-    fn ppq(&self) -> usize;
-    fn set_ppq(&mut self, ppq: usize);
-}
-
-#[derive(Debug, Copy, Clone)]
-#[cfg_attr(feature = "with_serde", derive(serde::Serialize, serde::Deserialize))]
-pub struct ClockData {
-    pub bpm: Float,
-    pub period_micros: Float,
-    pub ppq: usize,
-}
+use crate::{
+    clock::{Clock, ClockData},
+    Float,
+};
 
 pub struct ClockGetPeriodMicro<P>
 where
@@ -70,79 +50,6 @@ where
 {
     get: G,
     set: S,
-}
-
-macro_rules! period_micro {
-    ($bpm:expr, $ppq:expr) => {
-        60.0e6 / ($bpm * $ppq as Float)
-    };
-}
-
-/// A builder for ClockData that can happen in a static context.
-#[macro_export]
-macro_rules! make_clock {
-    ($bpm:expr, $ppq:expr) => {
-        crate::binding::bpm::ClockData {
-            bpm: $bpm,
-            period_micros: period_micro!($bpm, $ppq),
-            ppq: $ppq,
-        }
-    };
-}
-
-impl ClockData {
-    pub fn period_micro(bpm: Float, ppq: usize) -> Float {
-        period_micro!(bpm, ppq)
-    }
-
-    pub fn new(bpm: Float, ppq: usize) -> Self {
-        Self {
-            bpm,
-            period_micros: Self::period_micro(bpm, ppq),
-            ppq,
-        }
-    }
-}
-
-impl Default for ClockData {
-    fn default() -> Self {
-        let bpm = 120.0;
-        let ppq = 960;
-        Self {
-            bpm,
-            ppq,
-            period_micros: period_micro!(bpm, ppq),
-        }
-    }
-}
-
-impl Clock for ClockData {
-    fn bpm(&self) -> Float {
-        self.bpm
-    }
-
-    fn set_bpm(&mut self, bpm: Float) {
-        self.bpm = num_traits::clamp(bpm, BPM_MIN, Float::MAX);
-        self.period_micros = Self::period_micro(self.bpm, self.ppq);
-    }
-
-    fn period_micros(&self) -> Float {
-        self.period_micros
-    }
-
-    fn set_period_micros(&mut self, period_micros: Float) {
-        self.period_micros = num_traits::clamp(period_micros, PERIOD_MICRO_MIN, Float::MAX);
-        self.bpm = 60.0e6 / (self.period_micros * self.ppq as Float);
-    }
-
-    fn ppq(&self) -> usize {
-        self.ppq
-    }
-
-    fn set_ppq(&mut self, ppq: usize) {
-        self.ppq = num_traits::clamp(ppq, PPQ_MIN, usize::MAX);
-        self.period_micros = Self::period_micro(self.bpm, self.ppq);
-    }
 }
 
 impl<P> ClockGetPeriodMicro<P>
@@ -265,38 +172,11 @@ where
     }
 }
 
+/*
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn bpm_value_test() {
-        assert_eq!(5208f64, bpm::ClockData::period_micro(120.0, 96).floor());
-        assert_eq!(20833f64, bpm::ClockData::period_micro(120.0, 24).floor());
-
-        let mut c = bpm::ClockData::new(120.0, 96);
-        assert_eq!(5208f64, c.period_micros().floor());
-        assert_eq!(120f64, c.bpm());
-        assert_eq!(96, c.ppq());
-
-        c.set_ppq(24);
-        assert_eq!(20833f64, c.period_micros().floor());
-        assert_eq!(120f64, c.bpm());
-        assert_eq!(24, c.ppq());
-
-        c.set_bpm(2.0);
-        c.set_ppq(96);
-        assert_eq!(2f64, c.bpm());
-        assert_eq!(96, c.ppq());
-        assert_ne!(5208f64, c.period_micros().floor());
-
-        c.set_period_micros(5_208.333333f64);
-        assert_eq!(120f64, c.bpm().floor());
-        assert_eq!(96, c.ppq());
-        assert_eq!(5208f64, c.period_micros().floor());
-    }
-
-    /*
     #[test]
     fn bpm_binding_test() {
         let b: Arc<Mutex<dyn Clock>> = Arc::new(Mutex::new(bpm::ClockData::new(120.0, 96)));
@@ -382,5 +262,5 @@ mod tests {
         assert_eq!(5208f64, c.lock().period_micros().floor());
         assert_eq!(5208f64, micros.get().floor());
     }
-    */
 }
+*/
