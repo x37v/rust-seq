29e19f69bdfeef944ac2eae80bfa4678be056de5
Alex Norman <alex@x37v.info>
2021-12-24 14:32:35 -0800

    step sequencer GET index, not set
    should allow random and more easily syncing etc

--- src/graph/node/step_seq.rs
+++ src/graph/node/step_seq.rs
@@ -1,58 +1,45 @@
 use crate::{
     event::EventEvalContext,
     graph::{GraphChildExec, GraphNodeExec},
-    param::{ParamGet, ParamSet},
+    param::ParamGet,
 };
 
 /// A step sequencer node
-pub struct StepSeq<StepTicks, Steps, Index, const INDEX_CHILDREN: bool>
+pub struct StepSeq<StepTicks, Index, const INDEX_CHILDREN: bool>
 where
     StepTicks: ParamGet<usize>,
-    Steps: ParamGet<usize>,
-    Index: ParamSet<usize>,
+    Index: ParamGet<usize>,
 {
     step_ticks: StepTicks,
-    steps: Steps,
     index: Index,
 }
 
-impl<StepTicks, Steps, Index, const INDEX_CHILDREN: bool>
-    StepSeq<StepTicks, Steps, Index, INDEX_CHILDREN>
+impl<StepTicks, Index, const INDEX_CHILDREN: bool> StepSeq<StepTicks, Index, INDEX_CHILDREN>
 where
     StepTicks: ParamGet<usize>,
-    Steps: ParamGet<usize>,
-    Index: ParamSet<usize>,
+    Index: ParamGet<usize>,
 {
-    pub fn new(step_ticks: StepTicks, steps: Steps, index: Index) -> Self {
-        Self {
-            step_ticks,
-            steps,
-            index,
-        }
+    pub fn new(step_ticks: StepTicks, index: Index) -> Self {
+        Self { step_ticks, index }
     }
 }
 
-impl<StepTicks, Steps, Index, E, const INDEX_CHILDREN: bool> GraphNodeExec<E>
-    for StepSeq<StepTicks, Steps, Index, INDEX_CHILDREN>
+impl<StepTicks, Index, E, const INDEX_CHILDREN: bool> GraphNodeExec<E>
+    for StepSeq<StepTicks, Index, INDEX_CHILDREN>
 where
     StepTicks: ParamGet<usize>,
-    Steps: ParamGet<usize>,
-    Index: ParamSet<usize>,
+    Index: ParamGet<usize>,
     E: Send,
 {
     fn graph_exec(&self, context: &mut dyn EventEvalContext<E>, children: &dyn GraphChildExec<E>) {
         let step_ticks = self.step_ticks.get();
 
         if step_ticks > 0 && context.context_tick_now() % step_ticks == 0 {
-            let steps = self.steps.get();
-            if steps > 0 {
-                let index = (context.context_tick_now() / step_ticks) % steps as usize;
-                self.index.set(index);
-                if INDEX_CHILDREN {
-                    children.child_exec(context, index);
-                } else {
-                    children.child_exec_all(context);
-                }
+            let index = self.index.get();
+            if INDEX_CHILDREN {
+                children.child_exec(context, index);
+            } else {
+                children.child_exec_all(context);
             }
         }
     }
