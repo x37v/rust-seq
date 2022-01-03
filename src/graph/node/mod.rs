75165f3f87e9c203169be9d78172fd6190849c77
Alex Norman <alex@x37v.info>
2022-01-02 16:30:39 -0800

    tick offset node

--- src/graph/node/mod.rs
+++ src/graph/node/mod.rs
@@ -3,3 +3,4 @@ pub mod gate;
 pub mod param_store;
 pub mod repeat;
 pub mod step_seq;
+pub mod tick_offset;
---
+++ src/graph/node/tick_offset.rs
@@ -0,0 +1,35 @@
+use crate::{
+    context::ChildContext,
+    event::EventEvalContext,
+    graph::{GraphChildExec, GraphNodeExec},
+    param::ParamGet,
+    tick::offset_tick,
+};
+
+//only applies offset to context
+pub struct TickOffset<T> {
+    offset: T,
+}
+
+impl<T> TickOffset<T>
+where
+    T: ParamGet<isize>,
+{
+    pub fn new(offset: T) -> Self {
+        Self { offset }
+    }
+}
+
+impl<T, E> GraphNodeExec<E> for TickOffset<T>
+where
+    T: ParamGet<isize>,
+    E: Send,
+{
+    fn graph_exec(&self, context: &mut dyn EventEvalContext<E>, children: &dyn GraphChildExec<E>) {
+        let period_micros = context.context_tick_period_micros();
+        let context_tick = offset_tick(context.context_tick_now(), self.offset.get());
+
+        let mut ccontext = ChildContext::new(context, 0, context_tick, period_micros);
+        children.child_exec_all(&mut ccontext);
+    }
+}
