309767c1e3913a908b35454695367da7f1c24a50
Alex Norman <alex@x37v.info>
2023-05-20 16:38:44 -0700

    add ref array

--- src/graph/children.rs
+++ src/graph/children.rs
@@ -37,3 +37,37 @@ pub mod boxed {
         }
     }
 }
+
+use crate::{
+    event::EventEvalContext,
+    graph::{ChildCount, GraphChildExec, GraphNode},
+};
+
+/// An array of children
+pub struct GraphChildrenArray<'a, E, U, const N: usize>([&'a dyn GraphNode<E, U>; N]);
+
+impl<'a, E, U, const N: usize> GraphChildrenArray<'a, E, U, N> {
+    pub fn new(children: [&'a dyn GraphNode<E, U>; N]) -> Self {
+        Self(children)
+    }
+}
+
+impl<'a, E, U, const N: usize> GraphChildExec<E, U> for GraphChildrenArray<'a, E, U, N> {
+    fn child_count(&self) -> ChildCount {
+        if N == 0 {
+            ChildCount::None
+        } else {
+            ChildCount::Some(N)
+        }
+    }
+    fn child_exec_range(
+        &self,
+        context: &mut dyn EventEvalContext<E>,
+        range: core::ops::Range<usize>,
+        user_data: &mut U,
+    ) {
+        for c in self.0.iter().skip(range.start).take(range.len()) {
+            c.node_exec(context, user_data);
+        }
+    }
+}
